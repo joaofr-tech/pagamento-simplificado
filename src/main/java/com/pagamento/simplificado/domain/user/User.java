b041bed1458b813b00857d157bcc70fc7d880ee0
João Francisco <JF756061@gmail.com>
2026-05-24 20:13:22 -0300

    feat: create User entity

---
+++ src/main/java/com/pagamento/simplificado/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.pagamento.simplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table (name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/pagamento/simplificado/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.pagamento.simplificado.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
