fb0548abdd9d2259a0d4107a1b39d06c0b060a78
João Francisco <JF756061@gmail.com>
2026-05-24 21:15:59 -0300

    feat: create NotificationService

---
+++ src/main/java/com/pagamento/simplificado/service/NotificationService.java
@@ -0,0 +1,27 @@
+package com.pagamento.simplificado.service;
+
+import com.pagamento.simplificado.domain.User;
+import com.pagamento.simplificado.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("erro ao enviar notificacao");
+            throw new Exception("Servico de notificacao esta fora do ar");
+        }
+    }
+}
