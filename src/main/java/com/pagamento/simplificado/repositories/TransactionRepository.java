ee54dd7acdc3e9fc2a7a691a30be4ddbcb2ac985
João Francisco <JF756061@gmail.com>
2026-05-24 21:15:23 -0300

    feat: create TransactionDTO

--- src/main/java/com/pagamento/simplificado/repositories/TransactionRepository.java
+++ src/main/java/com/pagamento/simplificado/repositories/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.pagamento.simplificado.repositories;
 
-import com.pagamento.simplificado.domain.user.Transaction;
+import com.pagamento.simplificado.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
