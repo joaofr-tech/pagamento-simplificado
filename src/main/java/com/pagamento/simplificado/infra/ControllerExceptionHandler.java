60dce2a8b2db13f4d5294d0135b16930a9b831c1
João Francisco <JF756061@gmail.com>
2026-06-27 14:37:29 -0300

    fix: corrigir ControllerExceptionHandler para tratar Exception generica

--- src/main/java/com/pagamento/simplificado/infra/ControllerExceptionHandler.java
+++ src/main/java/com/pagamento/simplificado/infra/ControllerExceptionHandler.java
@@ -23,7 +23,7 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-        public ResponseEntity<ExceptionDTO> threatGeneralException(EntityNotFoundException exception){
+        public ResponseEntity<ExceptionDTO> threatGeneralException(Exception exception){
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
         return ResponseEntity.internalServerError().body(exceptionDTO);
     }
